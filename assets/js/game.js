b95b73cace9c8c765e670e8af37344e8eae51ba8
0ffpudding <jennaanderson00@gmail.com>
2022-03-08 18:48:26 -0600

    added functions for player and enemy health

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,10 +18,24 @@ console.log(
     playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
 );
 
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+
 playerHealth = playerHealth - enemyAttack;
 
 console.log(
     enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
 );
+
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
 
 fight();
